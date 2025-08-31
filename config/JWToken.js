a49f93d35233248da3087f63cbbc0dbc4d9bda70
Yajij Arafat <126899438+yajij-ok@users.noreply.github.com>
2025-09-01 00:39:14 +0600

    Update JWToken.js

--- config/JWToken.js
+++ config/JWToken.js
@@ -3,12 +3,12 @@ const jwt = require ('jsonwebtoken')
 const generateToken =(id) =>{
     return jwt.sign({id }, process.env.JWT_SECRET, {expiresIn: "3d"});
 }
-
+const isProduction = process.env.NODE_ENV === "production";
 const setAccessTokenCookie = (res, token) => {
     res.cookie("accessToken", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV=== "production",
-        sameSite: "None",
+        secure: isProduction,
+        sameSite: isProduction ? "None" : "Lax",
          path: "/",
         maxAge: 15 * 60 * 1000, // 15 minutes
     });
